061d6c6edbc5a24369d6263ca15c2d12b3e57e61
PhucLanPhan <phuclan.phan@my.jcu.edu.au>
2022-09-23 10:01:48 +1000

    create the package TextIO

---
+++ src/lec/TestTextIO.java
@@ -0,0 +1,22 @@
+package lec;
+
+import textio.TextIO;
+
+public class TestTextIO {
+    public static void main(String[] args) {
+        //start new input
+        TextIO.put("Enter name: ");
+        //string input
+        String name = TextIO.getln();
+        //numerical input
+        TextIO.put("Enter age: ");
+        int age = TextIO.getlnInt();
+        TextIO.put("Enter salary: ");
+        double salary = TextIO.getlnDouble();
+
+        // output to standard output
+        TextIO.putln("Name: " + name);
+        TextIO.putln("Age: " + age);
+        TextIO.putln("Income: " + salary);
+    }
+}
